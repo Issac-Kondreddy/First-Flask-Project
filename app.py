6d7098abf97357e5e3f51887e5addd8671dc3fad
issac-kondreddy <ixk93660@ucmo.edu>
2025-03-26 21:15:37 -0500

    Introduction

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-@app.route('/submit',methods=['POST'])
-def submit():
-    name = request.form['name']
-    return f"Hello, {name}"
+@app.route('/hello')
+def hello_world():
+    return "Hello This is Issac!"
+@app.route('/hello/<name>')
+def hello_name(name):
+    return f"Hello {name}"
 if __name__ == "__main__":
     app.run(debug=True)
 
